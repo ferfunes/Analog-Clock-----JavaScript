9616c6ab095d4989dcb50dba7c666a7f7a14a4e3
ferfunes <ferfunezdev@gmail.com>
2020-05-07 12:25:50 -0400

    created containers to store hours, minutes and seconds degrees

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+//Contains the degrees for each arm
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 267;
+
+//Aplying the degrees into the inline css style
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
